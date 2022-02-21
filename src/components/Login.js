a99e0b5bf1fddaabbe859a57533ce9c5fda996cf
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-02-21 23:01:28 +0530

    Major Project: Routing and Auth->Form handling: Controlled comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,34 @@
+// we can use uncontrolled componenet on when we have to use DOm like animation etc hover
+
 import React, { Component } from 'react';
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passInputRef = React.createRef();
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log('email input form', this.emailInputRef);
-    console.log('Password input form', this.passInputRef);
+    // console.log('email input form', this.emailInputRef);
+    // console.log('Password input form', this.passInputRef);
+    console.log('this.state', this.state);
+  };
+  handleEmailChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.value,
+    });
   };
   render() {
     return (
@@ -20,7 +39,8 @@ class Login extends Component {
             type="email"
             placeholder="Email"
             required
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
           />
         </div>
         <div className="field">
@@ -28,7 +48,8 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             required
-            ref={this.passInputRef}
+            // ref={this.passInputRef}
+            onChange={this.handlePasswordChange}
           />
         </div>
         <div className="field">
