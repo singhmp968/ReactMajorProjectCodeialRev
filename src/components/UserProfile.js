7b15c9fd756c333f4f6e57f453ac95ee02817a4b
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-03-06 12:22:57 +0530

    Major Project: User profile ->Mini assignments solutions: adding user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -14,8 +14,39 @@ class UserProfile extends Component {
 
   render() {
     console.log('insidecdm==>', this.props);
+    const {
+      match: { params },
+      profile,
+    } = this.props;
+    const user = profile.user;
+    if (profile.inProgress) {
+      return <h1>Loading...</h1>;
+    }
 
-    return <div>hi man</div>;
+    return (
+      <div className="settings">
+        <div className="img-container">
+          <img
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-dp"
+          />
+        </div>
+
+        <div className="field">
+          <div className="field-label">Name</div>
+          <div className="field-value">{user.name}</div>
+        </div>
+
+        <div className="field">
+          <div className="field-label">Email</div>
+          <div className="field-value">{user.email}</div>
+        </div>
+
+        <div className="btn-grp">
+          <button className="button save-btn">Add Friend</button>
+        </div>
+      </div>
+    );
   }
 }
 
