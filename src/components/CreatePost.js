24a0e4a56da13d1a2694d38848e80f48091323e0
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-03-07 21:57:13 +0530

    Major Project: Search and Chat->Creating Post: UI

---
+++ src/components/CreatePost.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+  }
+  handleOnClick = () => {
+    // dispatch actio
+  };
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="create-post">
+        <textarea
+          className="add-post"
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+        <div>
+          <button id="add-post-btn" onClick={this.handleOnClick}>
+            Add post
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreatePost;
