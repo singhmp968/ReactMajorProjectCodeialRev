b0d09b91e8470d9fd95f5bbe7c587a64d7662fc6
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-03-10 22:35:53 +0530

    Major Project: Search and Chat->Chat: using sockets

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../chat.css';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
 
 class Chat extends Component {
   constructor(props) {
@@ -9,7 +11,97 @@ class Chat extends Component {
       messages: [], // {content: 'some message', self: true}
       typedMessage: '',
     };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    this.socket.on('connect', function () {
+      console.log('CONNECTION ESTABLISHED');
+
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) {
+        console.log('NE USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) {
+      // add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
+
+  //   setUpConnection = () => {
+  //     const socketConnection = this.socket;
+  //     const self = this;
+  //     this.socket.on('connect', function () {
+  //       console.log('CONNECTION ESTABLISHED');
+  //       socketConnection.emit('join_room', {
+  //         user_email: this.userEmail,
+  //         chatroom: 'codeial',
+  //       });
+  //       socketConnection.on('user_joined', function (data) {
+  //         console.log('NEW USER JOINED');
+  //       });
+  //     });
+  //     this.socket.on('receive_message', function (data) {
+  //       // add message to state
+  //       const { messages } = self.state;
+  //       const messageObject = {};
+  //       messageObject.content = data.message;
+  //       if (data.user_email === self.userEmail) {
+  //         messageObject.self = true;
+  //       }
+  //       self.setState({
+  //         messages: [...messages, messageObject],
+  //         typedMessage: '',
+  //       });
+  //     });
+  //   };
+  //   handleSubmit = () => {
+  //     const { typedMessage } = this.state;
+  //     if (typedMessage && this.userEmail) {
+  //       this.socket.emit('send_message', {
+  //         message: typedMessage,
+  //         user_email: this.userEmail,
+  //         chatroom: 'codeial',
+  //       });
+  //     }
+  //   };
   render() {
     const { typedMessage, messages } = this.state;
     return (
@@ -48,5 +140,9 @@ class Chat extends Component {
     );
   }
 }
-
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(Chat);
